bfa1ab35621690f1f1a1d1f9d8cc1c0fd89b99d9
footprinthere <tai15515@kakao.com>
2024-05-15 16:39:57 +0900

    Implement GPT agent

---
+++ model/gpt_agent.py
@@ -0,0 +1,81 @@
+from typing import Literal
+
+from openai.types.chat.chat_completion_message_param import ChatCompletionMessageParam
+from chromadb import Collection
+
+from tools import log
+from .client import openai_client
+from .model_name import ModelName
+from .prompts import CHATBOT_ANSWER
+
+
+class BaseGPTAgent:
+
+    def __init__(self, model: ModelName):
+        self.model = model
+        self._messages: list[ChatCompletionMessageParam] = []
+
+    def _add_message(self, role: Literal["system", "user"], content: str) -> None:
+        self._messages.append({"role": role, "content": content})  # type: ignore
+
+    def _clear_message(self) -> None:
+        self._messages.clear()
+
+    def _call_api(self, temperature: float) -> str:
+        if len(self._messages) == 0:
+            raise ValueError("Messages should be added before calling API")
+
+        response = openai_client.chat.completions.create(
+            model=self.model,
+            messages=self._messages,
+            temperature=temperature,
+        )
+        answer = response.choices[0].message.content
+        if answer is None:
+            raise ValueError("Model did not output any message")
+        return answer
+
+
+class ChatbotAgent(BaseGPTAgent):
+
+    prompt = CHATBOT_ANSWER
+
+    def __init__(self, model: ModelName, db_collection: Collection):
+        super().__init__(model=model)
+        self.db_collections = db_collection
+
+        self._question_history: list[str] = []
+
+    def answer(
+        self,
+        question: str,
+        n_retrievals: int = 1,
+        verbose: bool = False,
+    ) -> str:
+        self._question_history.append(question)
+        parsed_question = self.parse_history()
+
+        retrieved = self.retrieve_info(question=parsed_question, n_results=n_retrievals)
+        message = ChatbotAgent.prompt.format(
+            question=parsed_question, retrieved=retrieved
+        )
+        if verbose:
+            log(f"--- Retrieved Information ---\n{retrieved}")
+            log(f"--- Prompt ---\n{message}")
+
+        self._add_message(role="user", content=message)
+        return self._call_api(temperature=0.1)
+
+    def retrieve_info(self, question: str, n_results: int = 1) -> str:
+        result = self.db_collections.query(query_texts=[question], n_results=n_results)
+        metadatas = result["metadatas"]
+        assert metadatas is not None
+
+        contents: list[str] = [m["content"] for m in metadatas[0]]  # type: ignore
+        return "\n".join(contents)
+
+    def clear_history(self) -> None:
+        self._question_history.clear()
+
+    def parse_history(self) -> str:
+        return "\n".join(self._question_history)
--- model/model_name.py
+++ model/model_name.py
@@ -4,3 +4,5 @@ from enum import Enum
 class ModelName(str, Enum):
     EMBEDDING_SMALL = "text-embedding-3-small"
     EMBEDDING_ADA = "text-embedding-ada-002"
+
+    GPT_3_5_1106 = "gpt-3.5-turbo-1106"

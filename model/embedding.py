716dc209b2c09527ea896a147507576720cb9d01
footprinthere <tai15515@kakao.com>
2024-05-16 19:45:18 +0900

    Remove unused function

--- model/embedding.py
+++ model/embedding.py
@@ -4,10 +4,5 @@ from .client import OPENAI_API_KEY, openai_client
 from .model_name import ModelName
 
 
-def embed(inputs: list[str], model: ModelName) -> list[list[float]]:
-    response = openai_client.embeddings.create(input=inputs, model=model)
-    return [e.embedding for e in response.data]
-
-
 def get_chroma_embedding_function(model: ModelName) -> OpenAIEmbeddingFunction:
     return OpenAIEmbeddingFunction(model_name=model, api_key=OPENAI_API_KEY)

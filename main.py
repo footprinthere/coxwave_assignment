dbf14dc356bef43405a3c5f5ec96094285b4a6ba
footprinthere <tai15515@kakao.com>
2024-05-16 20:15:09 +0900

    Refactor main code

--- main.py
+++ main.py
@@ -8,13 +8,15 @@ from model import (
     ModelName,
     EmptyMessageError,
 )
-from tools import log, ChatbotCycleResult
+from tools import log
 
 
 class Main:
 
     agent: ChatbotAgent
     args: argparse.Namespace
+
+    retry_count = 0
 
     @classmethod
     def main(cls):
@@ -34,18 +36,10 @@ class Main:
         print(cls.START_MESSAGE)
 
         # Run chatbot
-        retry_count = 0
         while True:
             user_input = input("\033[36m>>>\033[0m ").strip()
-            result = cls._cycle(user_input, retry_count)
-            if cls.args.debug:
-                log(f"Cycle result: {result}")
-
-            if result == ChatbotCycleResult.OK:
-                retry_count = 0
-            elif result == ChatbotCycleResult.RETRY:
-                retry_count += 1
-            elif result == ChatbotCycleResult.EXIT:
+            cont = cls._cycle(user_input)
+            if not cont:
                 break
 
     @classmethod
@@ -78,46 +72,51 @@ class Main:
         return agent
 
     @classmethod
-    def _cycle(cls, user_input: str, retry_count: int) -> ChatbotCycleResult:
+    def _cycle(cls, user_input: str) -> bool:
+        """Returns True if the chatbot should continue, False otherwise."""
+
         if user_input == "":
-            return ChatbotCycleResult.INVALID
+            return True
         if user_input == "!종료":
-            return ChatbotCycleResult.EXIT
+            return False
         if user_input.startswith("!") and user_input != "!재시도":
-            print("잘못된 명령어입니다.")
-            return ChatbotCycleResult.INVALID
+            cls._bot_message("이해할 수 없는 명령어입니다.")
+            return True
 
         if user_input == "!재시도":
-            if retry_count >= cls.args.max_retry:
-                print("최대 횟수를 초과해 더 이상 재시도할 수 없습니다.")
-                return ChatbotCycleResult.INVALID
-            try:
-                answer = cls.agent.answer(
-                    n_retrievals=2 * retry_count + 3,
-                    verbose=cls.args.debug,
-                )
-            except EmptyMessageError:
-                print("입력된 질문이 없어 재시도할 수 없습니다.")
-                return ChatbotCycleResult.INVALID
-            else:
-                result = ChatbotCycleResult.RETRY
+            cls.retry_count += 1
+            if cls.args.debug:
+                log(f"Retry count: {cls.retry_count}")
+            if cls.retry_count > cls.args.max_retry:
+                cls._bot_message("최대 횟수를 초과해 더 이상 재시도할 수 없습니다.")
+                return True
+            question = None  # Retry with the same question
         else:
+            cls.retry_count = 0
+            question = user_input
+
+        try:
             answer = cls.agent.answer(
-                question=user_input,
-                n_retrievals=2 * retry_count + 1,  # 1, 3, 5, ...
+                question=question,
+                n_retrievals=2 * cls.retry_count + 1,  # 1, 3, 5, ...
                 verbose=cls.args.debug,
             )
-            result = ChatbotCycleResult.OK
+        except EmptyMessageError:
+            cls._bot_message("입력된 질문이 없어 재시도할 수 없습니다.")
+            return True
 
         if answer.upper().startswith("X"):
-            print(
-                "🤖 죄송합니다. 스마트스토어와 관련이 없는 질문에는 답변해드릴 수 없어요.\n"
-            )
             cls.agent.clear_history()
-            return ChatbotCycleResult.UNRELATED
-        else:
-            print(f"🤖 {answer}\n")
-            return result
+            answer = (
+                "죄송합니다. 스마트스토어와 관련이 없는 질문에는 답변해드릴 수 없어요."
+            )
+
+        cls._bot_message(answer)
+        return True
+
+    @classmethod
+    def _bot_message(cls, message: str):
+        print(f"🤖 {message}\n")
 
     START_MESSAGE = (
         "스마트스토어 챗봇을 시작합니다. 스마트스토어에 관한 질문을 입력하시면 FAQ 기록을 바탕으로 답변해드립니다.\n"
